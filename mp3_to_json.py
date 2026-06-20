16f48aff6b93e95304889209bb13dd57695797a6
Akki Mujawdiya <mujawdiyaakki6261@gmail.com>
2026-06-20 16:29:04 +0530

    Remove commented-out code in mp3_to_json.py

--- mp3_to_json.py
+++ mp3_to_json.py
@@ -1,39 +1,3 @@
-# import whisper
-# import json
-# import os
-
-# model = whisper.load_model("large-v2")
-
-# audios= os.listdir("audios")
-# for audio in audios:
-#     print(audio)
-#     if("_" in audio):
-        
-#         number= audio.split("_")[0]
-#         title=audio.split("_")[1][:-4]
-#         print(number,title)
-        
-#         result= model.transcribe(audio = f"audios/{audio}",
-#                         # FP16=False,
-                        
-#         # result= model.transcribe(audio = f"audios/output.mp3 ",
-#                          language="hi",
-#                          task="translate",
-#                          word_timestamps=False)
-        
-#         chunks=[]
-#         for segment in result["segments"]:
-#             chunks.append({"number" : number, "title" : title, "start": segment["start"], "end":segment["end"], "text": segment["text"]})
-
-#         chunks_with_metadata={"chunks":chunks, "text" :result["text"]}
-#          # print(chunks)
-
-
-#         with open(f"jsons/{audio}.json","w") as f:
-#             json.dump(chunks_with_metadata, f)
-             
-             
-             
 import whisper
 import json
 import os
